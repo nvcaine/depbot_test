5e75318abfcc0050de75e01432c2915bb3269e72
Romuald Halasz <romuald.halasz@wikitude.com>
2021-02-03 16:20:59 +0100

    Add old logger module

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 'use strict';
 
 const express = require('express');
+const logger = require('simple-node-logger').createSimpleLogger();
+const url = require('url');
 
 const PORT = 8088;
 const HOST = '0.0.0.0';
 const app = express();
 
 const randomNumber = Math.floor(Math.random() * 1000);
+
+app.use((req, res, next) => {
+	const urlData = url.parse(req.url);
+	logger.info(`Received request: ${req.method} ${urlData.pathname}`);
+	next();
+});
 
 app.get('/', (req, res) => {
     res.status = 200;
@@ -22,4 +30,4 @@ app.get('/number', (req, res) => {
 
 app.listen(PORT, HOST);
 
-console.log(`Running on http://${HOST}:${PORT}`);
+logger.info(`Running on http://${HOST}:${PORT}`);
